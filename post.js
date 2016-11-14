0582f03289cc01fa6f5dd5951d8057671f82ddda
inizio <digit4free@gmail.com>
2016-11-14 20:05:33 +0800

    add title by default

--- post.js
+++ post.js
@@ -7,11 +7,14 @@ var sanitize = require('sanitize-filename')
 var now = moment().format('YYYY-MM-DD')
 
 var post_name = ''
-var current_subjects = ['math2111', 'comp2012', 'comp2611', 'comp2711', 'comp2611']
+var current_subjects = ['math2111', 'comp2012', 'comp2611', 'comp2711']
 
 var questions = [{
   name: 'title',
   message: 'Post Title: ',
+}, {
+  name: 'desc',
+  message: 'Description: ',
 }, {
   name: 'category',
   message: 'Category: ',
@@ -35,6 +38,7 @@ inquirer.prompt(questions).then(function(answers) {
   answers['title'] = capitalize(answers['title'])
   answers['date'] = now
   answers['filename'] = sanitize(answers['date'] + '-' + answers['title'])
+  answers['desc'] = answers['desc']
 
   // yaml front matter from
   var front_matter = '---\n'
@@ -47,7 +51,7 @@ inquirer.prompt(questions).then(function(answers) {
       front_matter = front_matter.slice(0, -1) + ' ]\n'
     } else front_matter += key + ': ' + answers[key] + '\n'
   }
-  front_matter += '---\n\n'
+  front_matter += '---\n\n# ' + answers['title']
 
   createFile('./posts/' + answers['filename'] + '.md', front_matter, function(err) {
     if (typeof(err) !== 'undefined' && err !== '') {
